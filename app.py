5f2dcf1808292e5488e0af193e8e9a35f4fa333a
SalmaElhassika <69520963+SalmaElhassika@users.noreply.github.com>
2022-09-17 16:26:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -591,18 +591,3 @@ for variable in dir():
         del globals()[variable]
 del variable
     
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-You have no unread notifications
